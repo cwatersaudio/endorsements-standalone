3fe05aeb35938b0cc3f181b8f894c59ff0780558
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-11-30 11:05:52 -0500

    added preliminary input section

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -3,10 +3,20 @@ import React from "react"
 export default function Input (props) {
 
     return (
-        <>
-            
+        <div className="input--container">
+            <textarea>
+                {/* Endorsement text area here
+                Need to initialize state and make some  functions */}
+                Some placeholder text texttexttexttexttexttexttexttexttexttexttexttext
+                texttexttexttexttexttexttexttext
+                texttexttexttexttext
+                texttexttexttexttext
+            </textarea>
+            <button id="submitButton">
+                Publish
+            </button>
         
-        </>
+        </div>
 
     )
 
