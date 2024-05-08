73ffdea6731a16f6bf9c890d444192df14e55bc2
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2024-05-08 16:38:46 -0400

    added .mail

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "firebase/firestore"
+import { getFirestore, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBizmo7k7dwWBrrF-sD8Z1vz6dRsEHpoPo",
-  authDomain: "endorsements-aa2b6.firebaseapp.com",
-  projectId: "endorsements-aa2b6",
-  storageBucket: "endorsements-aa2b6.appspot.com",
-  messagingSenderId: "227298825249",
-  appId: "1:227298825249:web:86967bff4e9e9165a73f60"
+	apiKey: "",
+	authDomain: "endorsements-aa2b6.firebaseapp.com",
+	projectId: "endorsements-aa2b6",
+	storageBucket: "endorsements-aa2b6.appspot.com",
+	messagingSenderId: "227298825249",
+	appId: "1:227298825249:web:86967bff4e9e9165a73f60",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
-export const endorsementCollection = collection(db, "endorsements")
+const db = getFirestore(app);
+export const endorsementCollection = collection(db, "endorsements");
